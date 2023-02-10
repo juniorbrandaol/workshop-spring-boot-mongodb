96c58f96dae446c0ca36e7905ef4bbe4f694adc7
junior brandao <edilson_brandaojunior@hotmail.com>
2023-02-10 11:11:02 -0300

    Obtendo um post por id

---
+++ src/main/java/com/eblj/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.eblj.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eblj.workshopmongo.domain.Post;
+import com.eblj.workshopmongo.repository.PostRepository;
+import com.eblj.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+		
+}
